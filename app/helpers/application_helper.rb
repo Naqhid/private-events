64f23d9d2a21ce2103261af4456c7a38d7a5ea73
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-02-11 16:53:40 +0200

    rubocop fix linter error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,9 +39,7 @@ module ApplicationHelper
     if event.attendances.exists?(user_id: current_user.id)
       '<p>Already signed in</p>'.html_safe
     else
-      # rubocop:disable Metrics/LineLength
       (link_to 'Join', join_attendances_path(id: @event.id), class: 'btn btn-primary mb-4', method: :post).to_s.html_safe
-      # rubocop:enable Metrics/LineLength
     end
   end
 
